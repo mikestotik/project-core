3b01e01a8d62cbe0288950213b3f63d0d1deabd7
Mike Stotik <mikestotik@gmail.com>
2024-06-23 09:55:44 +0300

    Added md for roles

--- src/presentation/routes/role_routes.rs
+++ src/presentation/routes/role_routes.rs
@@ -1,11 +1,13 @@
 use actix_web::web;
 
 use crate::presentation::controllers::role_controller;
+use crate::presentation::middleware::auth_middleware::AuthMiddleware;
 
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::resource("/roles")
+            .wrap(AuthMiddleware)
             .route(web::get().to(role_controller::get_all_roles)),
     );
 }
